38f488a31bde06dc1d84569805f4b414824a7934
sidhuhurix <sidharth.s@hurix.com>
2023-08-16 11:03:02 +0530

    added instructions

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -8,20 +8,37 @@ import {
   Container,
   Typography,
 } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  Card: {
+    padding: theme.spacing(4),
+  },
+  CardMedia: {
+    // padding: "1em 2em",
+    height: "350px",
+    borderRadius: "100px",
+  },
+}));
 export default function Recipe() {
+  const className = useStyles();
   const { id } = useParams();
   console.log(id);
   const Recipes = recipes.find((item) => item.Id == id);
   console.log(Recipes);
   return (
-    <Container maxWidth="md">
-      <Card>
+    <Container maxWidth="md" className={className.root}>
+      <Card className={className.Card}>
         <CardMedia
           component="img"
           image={Recipes.Image}
           title={Recipes.Title}
-          ingredients={Recipes.Ingredients}
+          // ingredients={Recipes.Ingredients}
+          className={className.CardMedia}
         />
         <CardContent>
           <Typography
@@ -36,7 +53,20 @@ export default function Recipe() {
             Ingredients:
           </Typography>
 
-          <div></div>
+          <Typography variant="body2" color="textSecondary" component="ol">
+            {Recipes.Ingredients.map((Ingredients, index) => (
+              <li key={index}>{Ingredients}</li>
+            ))}
+          </Typography>
+          <Typography variant="h6" component="h6">
+            Instructions:
+          </Typography>
+
+          <Typography variant="body2" color="textSecondary" component="ol">
+            {Recipes.Instructions.map((Instructions, index) => (
+              <li key={index}>{Instructions}</li>
+            ))}
+          </Typography>
         </CardContent>
       </Card>
     </Container>
